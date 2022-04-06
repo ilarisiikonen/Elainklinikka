21d97a58b929786e7878d67babd906d54a355382
Ilari Siikonen <e2101365@edu.vamk.fi>
2022-04-06 11:03:34 +0300

    kirjauduv1

--- Eläinklinikka/MainWindow.xaml.cs
+++ Eläinklinikka/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Eläinklinikka
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
