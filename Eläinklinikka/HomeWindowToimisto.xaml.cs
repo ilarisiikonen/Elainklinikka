a91b9ebd697dafea836fbbcf136c0e335e69bacf
Ilari Siikonen <e2101365@edu.vamk.fi>
2022-04-20 09:11:28 +0300

    toimistoassistentin ikkunaa hahmoteltu

--- Eläinklinikka/HomeWindowToimisto.xaml.cs
+++ Eläinklinikka/HomeWindowToimisto.xaml.cs
@@ -10,28 +10,25 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Eläinklinikka
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Window1 homewindowtoimisto = new Window1();
-            homewindowtoimisto.Show();
+            MainWindow mainwindow = new MainWindow();
+            mainwindow.Show();
             this.Close();
         }
-
-        string käyttäjä = TextBox
     }
 }
